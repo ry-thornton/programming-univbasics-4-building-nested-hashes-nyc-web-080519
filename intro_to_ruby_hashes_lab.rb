7b419eb490e79adbf272aee46daa4a7aff5893ed
ryanthornton628 <ryanthornton628@gmail.com>
2019-07-16 02:06:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -52,7 +52,7 @@ def monopoly_with_fourth_tier
           'mortgage_value' => '$400'
         },
         :shortline => {
-          'mortgage_value' =>
+          'mortgage_value' => ''
         }
         
       },
